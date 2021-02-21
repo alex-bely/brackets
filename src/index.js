636f7c3857b26625a59dc6d63eb6f0b42d0e4e58
alex-bely <alex.bely.420601@gmail.com>
2021-02-21 17:31:50 +0300

    feat: implement Brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('');
+  let openingBrackets = bracketsConfig.map(x => x[0]);
+  let closingBrackets = bracketsConfig.map(x => x[1]);
+
+  let stack = [];
+  let missedItems = [];
+  array.forEach(element => {
+
+    let isOpeningBracketEqualToClosed = openingBrackets.includes(element) && closingBrackets.includes(element);
+    if(isOpeningBracketEqualToClosed){
+      processSameBracketType(stack, element);
+    }
+    else{
+      processDifferentBracketType(openingBrackets, element, stack, closingBrackets, bracketsConfig, missedItems);
+    }
+
+
+  });
+
+  return stack.length === 0 && missedItems.length === 0;
+}
+
+function processDifferentBracketType(openingBrackets, element, stack, closingBrackets, bracketsConfig, missedItems) {
+  if (openingBrackets.includes(element)) {
+    stack.push(element);
+  } else if (closingBrackets.includes(element)
+    && stack[stack.length - 1] === bracketsConfig.find((el) => el[1] === element)[0]) {
+    stack.pop();
+  }
+  else {
+    missedItems.push(element);
+  }
+}
+
+function processSameBracketType(stack, element) {
+  if (stack.includes(element)) {
+    stack.pop(element);
+  } else {
+    stack.push(element);
+  }
 }
